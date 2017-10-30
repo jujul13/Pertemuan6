6e1a195fbd36ed8eba5eb5078d2d6b449e35d9b3
jujul13 <juliopalbe@gmail.com>
2017-10-30 07:16:11 +0700

    Establishing your works area
    Video-096

---
+++ includes/layouts/footer.php
@@ -0,0 +1,4 @@
+<div id = "footer">Copyright 2017, Widget Corp</div>
+
+</body>
+</html>
---
+++ public/manage_content.php
@@ -0,0 +1,22 @@
+<?php require_once("../includes/functions.php"); 
+<?php include("../includes/layouts/header.php"); 
+?>
+<html lang="en">
+	<head>
+	<title>Admin Pagetitle</title>
+	<link href="public.css" media="all" rel="stylesheet" type="text/css" />
+	</head>
+<body>
+
+<div id="main">
+	<div id="navigation">
+		&nbsp;
+	</div>
+	<div id="page">
+		<h2>Manage Content</h2>
+	
+	</div>
+</div>
+
+<?php include("../includes/layouts/footer.php"); ?>
+
