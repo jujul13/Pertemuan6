f3b20cab5237d3d59c58288110b9e3f1a9654ae3
jujul13 <juliopalbe@gmail.com>
2017-10-30 08:40:50 +0700

    Video-099
    Connecting the application to database

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,4 +1,15 @@
-<?php require_once("../includes/functions.php"); ?>
+<?php include("../includes/db_connection.php"); ?>
+<?php include("../includes/functions.php"); ?>
+<?php
+	//2. Perform database query
+	$query = "SELECT * ";
+	$query .= "FROM subjects ";
+	$query .= "WHERE visible = 1";
+	$query .= "ORDER BY position ASC";
+	$result = mysqli_query($connection, $query);
+	//Test if there was a query error
+	confirm_query($result);
+?>
 <?php include("../includes/layouts/header.php"); ?>
 
 <html lang="en">
@@ -10,13 +21,28 @@
 
 <div id="main">
 	<div id="navigation">
-		&nbsp;
+		<ul>
+		<?php
+		 //3. Use returnetd data (if any)
+		 while($subject = mysqli_fetch_assoc($result)) {
+			 //output data from each row
+		?>
+			<li><?php echo $subject["menu_name"] . " (" .
+			$subject["id"]. ")"; ?></li>
+		 <?php
+		 }
+		 ?>
+		 </ul>
 	</div>
 	<div id="page">
 		<h2>Manage Content</h2>
 	
 	</div>
 </div>
+<?php 
+	//4. Release returned data
+	mysqli_free_result($result);
+?>
 
 <?php include("../includes/layouts/footer.php"); ?>
 
