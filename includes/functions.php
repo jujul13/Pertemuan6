f2904ca936aa74e36cfc53e0d96cdf2b4eb30c99
Julio Palbe <32197126+jujul13@users.noreply.github.com>
2017-11-10 11:36:00 +0700

    Video-106 Function.php
    Refactoring the page selection

--- includes/functions.php
+++ includes/functions.php
@@ -6,28 +6,31 @@
 	
 
 	function find_pages_for_subject(){
+
+
 		
-	function find_all_subjects() {
+	function find_page_by_id($page_id) {
+		
 		
 	function find_subject_by_id($subject_id) {
-		global $connection;
-
-		$safe_subject_id = mysqli_real_escape_string ($connection, $subject_id);
-
-		$query = "SELECT * ";
-		$query .= "FROM subjects ";
-		$query .= "WHERE id = {$safe_subject_id}";
-		$query .= "LIMIT 1";
-		$subject_set = mysqli_query($connection, $query);
-		confirm_query($subject_set);
-		if($subject = mysqli_fetch_assoc($subject_set);
-		return $subject;
-	} else {
-		return null;
+		global $current_subject;
+		global $current_page;
+		
+		if(isset($_GET["subject"])){
+		$current_subject = find_subject_by_id($_GET["subject"]);
+		$current_page = null;
+	}
+	elseif (isset($_GET["page"])){
+		$current_subject = null;
+		$current_page = find_page_by_id($_GET["page"]);
+	}
+	else {
+		$current_subject = null;
+		$current_page = null;
 	}
 }
 
-function navigation($subject_id, $page_id) {
+function navigation($subject_array, $page_array) {
 
 }
 
@@ -41,7 +44,7 @@ function navigation($subject_id, $page_id) {
 	<?php
 	while ($subject = mysqli_fetch_assoc($subject_set)) {
 		$output .= "<li";
-			if ($subject["id"] == $subject_id) {
+			if ($subject_array && $subject["id"] == $subject_array["id"]) {
 				$output .= " class=\"selected\"";
 			}
 			$output .=">";
@@ -55,7 +58,7 @@ function navigation($subject_id, $page_id) {
 		 	while($page = mysqli_fetch_assoc($page_set)) {
 
 				$output .= "<li";
-		if ($page["id"] == $page_id){
+		if ($page_array && $page["id"] == $page_array["id"]){
 			$output .= " class=\"selected\""; 
 		}
 		$output .= ">";
