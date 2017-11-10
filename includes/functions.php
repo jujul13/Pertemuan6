c43b976bd4091f948f3f7fc3c73e7079565b8a82
Julio Palbe <32197126+jujul13@users.noreply.github.com>
2017-11-10 11:03:50 +0700

    Video-105 Functions.php
    Finding a subject in the database

--- includes/functions.php
+++ includes/functions.php
@@ -6,8 +6,30 @@
 	
 
 	function find_pages_for_subject(){
-	
+		
+	function find_all_subjects() {
+		
+	function find_subject_by_id($subject_id) {
+		global $connection;
+
+		$safe_subject_id = mysqli_real_escape_string ($connection, $subject_id);
+
+		$query = "SELECT * ";
+		$query .= "FROM subjects ";
+		$query .= "WHERE id = {$safe_subject_id}";
+		$query .= "LIMIT 1";
+		$subject_set = mysqli_query($connection, $query);
+		confirm_query($subject_set);
+		if($subject = mysqli_fetch_assoc($subject_set);
+		return $subject;
+	} else {
+		return null;
+	}
+}
+
 function navigation($subject_id, $page_id) {
+
+}
 
 
 	// navigation takes 2 arguments
