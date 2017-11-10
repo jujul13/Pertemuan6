66f3c8e02e1f6d1b43c0381e48c07ed4c0af9603
Julio Palbe <32197126+jujul13@users.noreply.github.com>
2017-11-10 11:34:59 +0700

    Video-106
    Refactoring the page selection

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,20 +1,9 @@
 <?php include("../includes/db_connection.php"); ?>
 <?php include("../includes/functions.php"); ?>
 <?php include("../includes/layouts/header.php"); ?>
-<?php
-	if(isset($_GET["subject"])){
-		$selected_subject_id = $_GET["subject"];
-		$selected_page_id = null;
-	}
-	elseif (isset($_GET["page"])){
-		$selected_page_id = $_GET["page"];
-		$selected_subject_id = null;
-	}
-	else {
-		$selected_subject_id =null;
-		$selected_page_id = null;
-	}
-?>
+<?php find_selected_page(); ?>
+	
+
 
 <html lang="en">
 	<head>
@@ -25,7 +14,8 @@
 
 <div id="main">
 	<div id="navigation">
-
+		<?php echo navigation($current_subject, $current_page); ?>
+	</div>
 		<ul class="subjects">
 
 	<?php $subject_set = find_all_subjecst(); ?>
@@ -93,14 +83,18 @@
 	</div>
 	
 	<div id="page">
-		<h2>Manage Content</h2>
-		<?php if ($selected_subject_id) { ?>
-		<?php $current_subject = find_subject_by_id($selected_subject_id);?>		
+		<?php if ($current_subject) { ?>
+		<h2>Manage Subject</h2>
+		
+				Menu name : <?php echo $current_subject["menu_name"]; ?> <br />
 
-		Menu name : <?php echo $current_subject["menu_name"]; ?> <br />
+		<?php } elseif ($selected_page_id) {
+			?>
+		
+		<h2>Manage Page</h2>
+		
+		Menu name : <?php echo $current_page["menu_name"]; ?> <br />
 
-		<?php } elseif ($selected_subject_id); ?><br />
-	<?<?php echo $selected_subject_id; ?>
 	<?php } else { ?>
 		Please select a subject or a page.
 	<?php } ?>
