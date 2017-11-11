68f54d8b858f82e1e985a4736c8d3dca55f6f5e6
Julio Palbe <32197126+jujul13@users.noreply.github.com>
2017-11-11 13:42:09 +0700

    Video-118 header.php
    Using context for a conditional code

--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -1,10 +1,19 @@
+<?php 
+	if(isset($layout_context)){
+		$layout_context = "public" ?>
+	}
+?>
+
+
 <html lang="en">
 	<head>
-	<title></title>
+	<title><?php if ($layout_context == "admin"){
+		echo "Admin";  } ?></title>
 	<link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
 	</head>
 <body>
 
  <div id="header">
-	<h1>Widget Corp</h1>
+	<h1>Widget Corp <?php if ($layout_context == "admin"){
+		echo "Admin";  } ?></h1>
 	</div>
