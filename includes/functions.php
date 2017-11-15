86ff0d5d7b50e89bd210463f6f34cc572d120063
Julio Palbe <32197126+jujul13@users.noreply.github.com>
2017-11-15 09:56:39 +0700

    Video-128 Functions.php
    Creating a login system

--- includes/functions.php
+++ includes/functions.php
@@ -76,13 +76,32 @@
 
 
 	}
-
 	function find_admin_by_id($admin_id){
 		global $connection;
 
+		$safe_admin_id = mysqli_real_escape_string($connection, $admin_id);
+
 		$query = "SELECT * ";
 		$query .= "FROM admins";
-		$query .= "WHERE id = {$safe_admin_id}";
+		$query .= "WHERE username = {$safe_admin_id}";
+		$query .= "LIMIT 1";
+		$admin_set = mysqli_query($connection, $query);
+		confirm_query($admin_set);
+		if($admin = mysqli_fetch_assoc($admin_set)){
+			return $admin;
+		} else{
+			return null;
+		}
+	}
+
+	function find_admin_by_username($username){
+		global $connection;
+
+		$safe_username = mysqli_real_escape_string($connection, $username);
+
+		$query = "SELECT * ";
+		$query .= "FROM admins";
+		$query .= "WHERE username = {$safe_username}";
 		$query .= "LIMIT 1";
 		$admin_set = mysqli_query($connection, $query);
 		confirm_query($admin_set);
@@ -303,6 +322,24 @@ function public_navigation($subject_array, $page_array) {
 		if ($hash === $existing_hash) {
 			return true;
 		} else {
+			return false;
+		}
+	}
+
+	function attempt_login($username, $password) {
+		$admin = find_admin_by_username($username);
+		if ($admin){
+			//found admin, now check password
+
+			if(password_check($password, $admin ["hashed_password"])){
+				return $admin;
+			} else{
+				//password does not match
+				return false;
+			}
+
+		} else{
+			//admin not found
 			return false;
 		}
 	}
